d741b07eeaa93b0e8048b0d356ce99b3fec63796
wendellxavier <wendell_xw20@hotmail.com>
2024-06-01 09:19:43 -0300

    "arrumando"

--- cliente/views.py
+++ cliente/views.py
@@ -4,7 +4,17 @@ from cabeleleiro.models import DadosCabeleleiro, Especialidades, TipoCabelos
 
 def home(request):
     if request.method == "GET":
+        cabeleleiro_filtrar = request.GET.get('cabeleleiro')
+        especialidade_filtrar = request.GET.getlist('especialidades')
+        tipoCabelos_filtrar = request.GET.getlist('tiposCabelos')
         cabeleleiros = DadosCabeleleiro.objects.all()
+        
+        if cabeleleiro_filtar:
+            cabeleleiros = cabeleleiros.filter(nome__icontains=cabeleleiro_filtrar)
+            
+        if especialidade_filtrar:
+            cabeleleiros = cabeleleiros.filter(especialidade_id__in=especialidade_filtrar)
+        
         especialidades = Especialidades.objects.all()
         tiposCabelos = TipoCabelos.objects.all()
         return render(request, 'home.html', {'cabeleleiros': cabeleleiros, 'especialidades': especialidades, 'tiposCabelos': tiposCabelos})
